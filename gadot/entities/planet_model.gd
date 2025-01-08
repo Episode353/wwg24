5b840dd026d19386d0442524a33f03b4867db156
JoeToscani <39446512+Episode353@users.noreply.github.com>
2025-01-08 10:59:01 -0500

    edit tbmaps info

--- gadot/entities/planet_model.gd
+++ gadot/entities/planet_model.gd
@@ -7,4 +7,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Rotate slightly each frame.
-	self.rotate_y(0.0005 * delta)
+	self.rotate_y(0.0007 * delta)
